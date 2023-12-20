57fac9c87000c755b9c09e22a92abb40bbd4c4a7
Gokul <gokulsrinivask2020@gmail.com>
2023-12-21 00:22:58 +0530

    second commit

--- project/authapp/views.py
+++ project/authapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    return render(request,'signup.html')
+
+def handleLogin(request):
+    return render(request,'handleLogin.html')
+
+def handleLogout(request):
+    return render(request,'handleLogout.html')
+
+
